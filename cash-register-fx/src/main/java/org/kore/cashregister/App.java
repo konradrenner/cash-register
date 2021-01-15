ff57340f2ae6245769988eb8fc51941cfacca10c
Konrad Renner <koni.renner@gmail.com>
2021-01-15 15:49:33 +0100

    manuel

--- cash-register-fx/src/main/java/org/kore/cashregister/App.java
+++ cash-register-fx/src/main/java/org/kore/cashregister/App.java
@@ -17,7 +17,8 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("main"), 1024, 768);
+        scene = new Scene(loadFXML("main"), 1280, 800);
+        scene.getStylesheets().add(getClass().getResource("/styles/main.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
     }
